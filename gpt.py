631247b66c2186708bee99a01ff0511e16034e2c
efiegel <efiegel01@gmail.com>
2024-09-29 21:28:50 -0500

    pared down parameters and training data size

--- gpt.py
+++ gpt.py
@@ -3,17 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
-max_iters = 5000
-eval_interval = 500
+batch_size = 12 # how many independent sequences will we process in parallel?
+block_size = 64 # what is the maximum context length for predictions?
+max_iters = 2000
+eval_interval = 125
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 200
-n_embd = 384
-n_head = 6
-n_layer = 6
-dropout = 0.2
+eval_iters = 20
+n_embd = 128
+n_head = 4
+n_layer = 4
+dropout = 0.0
 # ------------
 
 torch.manual_seed(1337)
